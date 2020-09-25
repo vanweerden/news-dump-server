c3df914635ee6b9b0cd0bb250c811c9537524941
vanweerden <ando.vanweerden@gmail.com>
2020-09-25 15:30:29 +0800

    Basic server working

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fetchNews = require('./fetch');
+const config = require('./config');
+const { port, hostname } = config.app;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
